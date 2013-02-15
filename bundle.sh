283ee9ec1f708ae553821382b81ce2ecd79f4c94
Bill Linux <bill.linux@laposte.net>
2013-02-15 18:56:33 -0400

    Modified bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-# Clones or updates the Git repositories specified in `./**/*.get` files.
+# Clones or updates the Git repositories specified in `./**/*.get` files,
+# starting from the most recently modified file down to the earliest one.
 
-find . -type f -name '*.get' -print | while read get; do
-  dir=${get%.get}
-  echo -n "$dir => "
-  if cd "$dir/.git/.." 2>/dev/null; then
-    git pull
-    cd - >/dev/null
-  else
+find . -type f -name '*.get' -print | xargs ls -t | while read get; do
     url=$(cat "$get")
-    git clone "$url" "$dir"
-  fi
+    dir=${get%.get}
+    echo -n "$dir => "
+    if cd "$dir/.git/.." 2>/dev/null; then
+        git remote set-url origin "$url"
+        git pull origin
+        cd - >/dev/null
+    else
+        git clone "$url" "$dir"
+    fi
 done
